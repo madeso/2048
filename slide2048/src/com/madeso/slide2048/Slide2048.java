926ad2e7baa3f7aa533f5d32b895bc5c0f37c3de
Gustav <sir.gustav.the.coder@gmail.com>
2014-04-04 12:59:40 +0200

    Added basic world drawing

--- slide2048/src/com/madeso/slide2048/Slide2048.java
+++ slide2048/src/com/madeso/slide2048/Slide2048.java
@@ -2,6 +2,7 @@ package com.madeso.slide2048;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,12 +10,12 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Slide2048 implements ApplicationListener {
 	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	private ShapeRenderer batch;
 	
 	@Override
 	public void create() {		
@@ -22,33 +23,35 @@ public class Slide2048 implements ApplicationListener {
 		float h = Gdx.graphics.getHeight();
 		
 		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		batch = new ShapeRenderer();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		texture.dispose();
 	}
+	
+	Color background = new Color(0xFAF8EFFF);
+	Color gameBackground = new Color(0xBBADA0FF);
 
+	float gameScale = 0.9f;
+	
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(background.r, background.g, background.b, background.a);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
+		batch.begin(ShapeType.Filled);
+		
+		float w = camera.viewportWidth;
+		float h = camera.viewportHeight;
+		
+		float size = Math.min(h, w)*gameScale;
+		
+		batch.setColor(gameBackground);
+		batch.rect((w-size)/2.0f-w/2, (h-size)/2 - h/2, size, size);
+		
 		batch.end();
 	}
 
